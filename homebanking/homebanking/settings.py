007290e9a2c1049bde40ded0966df5f60dc0a8dc
Emanuel Pesci <emanuelpesci@gmail.com>
2023-11-15 16:01:15 -0300

    ahi cree los modelos de la tabla del sprint 6

--- homebanking/homebanking/settings.py
+++ homebanking/homebanking/settings.py
@@ -82,8 +82,8 @@ WSGI_APPLICATION = 'homebanking.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'NAME': BASE_DIR / "itbank.db",
+    },
 }
 
 
