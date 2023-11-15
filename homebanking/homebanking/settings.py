90405c2905f41e5d98ffb47619a98b3b8888c6ef
Emanuel Pesci <emanuelpesci@gmail.com>
2023-11-15 11:31:31 -0300

    cree todos los modelos

--- homebanking/homebanking/settings.py
+++ homebanking/homebanking/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'login'
+    'login',
+    'clientes',
+    'cuentas',
+    'tarjetas',
+    'movimientos',
+    'prestamos',
 ]
 
 MIDDLEWARE = [
